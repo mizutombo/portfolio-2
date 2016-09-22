62f50829611e678821fed3f97e392506a3813842
Nathan Hugon <nthugon@gmail.com>
2016-09-22 12:53:00 -0700

    add event delegation to .handleMainNav method

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -2,7 +2,7 @@
 var projectView = {};
 
 projectView.handleMainNav = function () {
-  $('.main-nav .tab').on('click', function() {
+  $('.main-nav').on('click', '.tab', function() {
     var tabValue = $(this).attr('data-content');
     $('.tab-content').hide();
     $('#' + tabValue).fadeIn('slow');
